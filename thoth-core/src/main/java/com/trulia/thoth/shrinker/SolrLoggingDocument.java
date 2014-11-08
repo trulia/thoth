1c1a980ccdc97eacc8c72705094d8058ddbeca36
Damiano Braga <damiano.braga@gmail.com>
2014-11-07 23:44:09 -0800

    Adding shrinking

---
+++ thoth-core/src/main/java/com/trulia/thoth/shrinker/SolrLoggingDocument.java
@@ -0,0 +1,22 @@
+package com.trulia.thoth.shrinker;
+
+
+
+    /**
+ * User: dbraga - Date: 10/6/13
+ */
+public class SolrLoggingDocument {
+
+  public static final String ID = "id";
+  public static final String BODY = "body_s";
+  public static final String EXCEPTION = "exception_b";
+  public static final String DATE = "date_dt";
+  public static final String PARAMS = "params_s";
+  public static final String QTIME = "qtime_i";
+  public static final String STATUS = "status_s";
+  public static final String HITS = "hits_i";
+  public static final String BITMASK = "bitmask_s";
+  public static final String REQUESTS_IN_PROGRESS = "requestInProgress_i";
+
+
+}
