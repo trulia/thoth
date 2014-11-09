a0b4ed0297a62c2302bf257f74fd7367a2e4065f
Damiano Braga <damiano.braga@gmail.com>
2014-11-08 16:48:01 -0800

    Cleaned a bit, ready to final refactor

--- thoth-core/src/main/java/com/trulia/thoth/util/QueryUtil.java
+++ thoth-core/src/main/java/com/trulia/thoth/util/QueryUtil.java
@@ -4,6 +4,7 @@ package com.trulia.thoth.util;
  * User: dbraga - Date: 10/15/14
  */
 public class QueryUtil {
+  public static final int SLOW_QUERY_THRESHOLD = 100;
 
   /**
    * Identify if a query is slow / fast depending on qtime
@@ -11,6 +12,6 @@ public class QueryUtil {
    * @return true or false depending if fast or slow
    */
   public static boolean isSlowQuery(int qtime){
-    return (qtime > 100); // ms
+    return (qtime > SLOW_QUERY_THRESHOLD); // ms
   }
 }
