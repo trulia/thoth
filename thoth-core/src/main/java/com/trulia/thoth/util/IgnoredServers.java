41264d6e4fb3299339c61548eb6562f6778299fa
Damiano Braga <damiano.braga@gmail.com>
2014-11-30 19:47:20 -0800

    Adding IgnoredServers class that is gonna be shared between thoth-monitor and thoth-ml

---
+++ thoth-core/src/main/java/com/trulia/thoth/util/IgnoredServers.java
@@ -0,0 +1,45 @@
+package com.trulia.thoth.util;
+
+import com.trulia.thoth.pojo.ServerDetail;
+
+import java.util.ArrayList;
+
+/**
+ * User: dbraga - Date: 11/30/14
+ */
+public class IgnoredServers {
+  private ArrayList<ServerDetail> ignoredServerDetails;
+
+  /**
+   * Helper class to convert string representation of server list to array list of server details
+   * @param stringListOfIgnoredServers string representation of list of servers to ignore
+   * format of the string:
+   * multiple server details are separated by comma ,
+   * each server detail is separated by semicolon ;
+   * example:
+   * name-of-the-host1;123456;name-of-the-core1;name-of-the-pool1,name-of-the-host2;123456;name-of-the-core2;name-of-the-pool2
+   *
+   *
+   */
+  private void convertStringListToArraylist(String stringListOfIgnoredServers){
+    ignoredServerDetails = new ArrayList<ServerDetail>();
+    for (String ignoredServer: stringListOfIgnoredServers.split(",")){
+      String[] split = ignoredServer.split(";");
+      if (split.length % 4 == 0) ignoredServerDetails.add(
+          new ServerDetail(split[0], split[3], split[1], split[2])
+      );
+    }
+  }
+
+  public ArrayList<ServerDetail> getIgnoredServersDetail(){
+    return this.ignoredServerDetails;
+  }
+
+  /**
+   * Encapsulate the concept of ignored servers that are currently intercepted on Thoth
+   * @param ignoredServersString string representation of the ignored server list
+   */
+  public IgnoredServers(String ignoredServersString){
+    convertStringListToArraylist(ignoredServersString);
+  }
+}
